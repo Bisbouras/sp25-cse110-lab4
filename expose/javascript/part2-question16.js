20b40a9af9ab17ba69dfa6c1748f8c988bf79b29
Chris Enotiadis <157077700+Bisbouras@users.noreply.github.com>
2025-04-29 23:29:25 -0700

    Complete part2-question16.js

---
+++ expose/javascript/part2-question16.js
@@ -0,0 +1,12 @@
+let statistics = {
+  redCars: 21,
+  blueCars: 45,
+  greenCars: 12,
+  raceCars: 12,
+  blackCars: 40,
+  rareCars: 2,
+};
+
+for (const i in statistics) {
+  if (i.startsWith('r') || statistics[i] % 2 !== 0) {
+    console.log('${i}: ${statistics[i]}');
